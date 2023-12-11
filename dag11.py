6dfc06e23a402e4467ae6fb3e9b5f35b183327a7
PaulRoland <prvanderplas@gmail.com>
2023-12-11 07:09:39 +0100

    Add files via upload

--- dag11.py
+++ dag11.py
@@ -10,13 +10,14 @@ import time
 start_time = time.time_ns()
 
 def galaxy_dist(galaxy1,galaxy2):
-    return abs(galaxy1[1]-galaxy2[1])+abs(galaxy1[2]-galaxy2[2])
+    return [abs(galaxy1[1]-galaxy2[1])+abs(galaxy1[2]-galaxy2[2]),abs(galaxy1[3]-galaxy2[3])+abs(galaxy1[4]-galaxy2[4])]
 
 f = open("input_dag11.txt", "r")
 
-time_factor=1000000-1
+time_factor=1000000-1 #lines added by old galaxy expansion
+
 galaxy=0
-galaxy_list=list()
+
 
 galaxy_map=list()
 for i,line in enumerate(f):
@@ -24,7 +25,7 @@ for i,line in enumerate(f):
     galaxy_map.append(line)
 f.close()    
 
-#horizontal expansion
+#places of horizontal expansion
 hor_expansion=list()
 for j,c in enumerate(galaxy_map[0]):
     hor_line=''
@@ -34,11 +35,12 @@ for j,c in enumerate(galaxy_map[0]):
         hor_expansion.append(j)
     
 
-#get galaxies using vertical expansion on the fly  
+#get galaxies, and add vertical expansion
+galaxy_list=list()
 y_expansion=0  
 for i,line in enumerate(galaxy_map):
     if set(line) == {'.'}:
-        y_expansion=y_expansion+time_factor #keep track of y expansion
+        y_expansion=y_expansion+1 #keep track of y expansion
         continue
 
     for j,c in enumerate(line):
@@ -47,28 +49,25 @@ for i,line in enumerate(galaxy_map):
             x_expansion=0
             for hor in hor_expansion:
                 if j>hor:
-                    x_expansion=x_expansion+time_factor
+                    x_expansion=x_expansion+1
             
-            galaxy_list.append([galaxy,j+x_expansion,i+y_expansion])
+            galaxy_list.append([galaxy,j+x_expansion,i+y_expansion,j+x_expansion*time_factor,i+y_expansion*time_factor])
             galaxy=galaxy+1
 
 
 
 #list of galaxis in galaxy_list galaxy,x,y
-small_dist=999999
-iters=0
-total_distance=0
+total_distance1=0
+total_distance2=0
 
+#go through all unique pairs:
 for a in it.combinations(galaxy_list,2):
     galaxy1=a[0]
     galaxy2=a[1]    
-    iters=iters+1
     distance = galaxy_dist(galaxy1,galaxy2)
-    print(galaxy1,galaxy2,distance)
-    total_distance=total_distance+distance
-    
-    if distance < small_dist:
-        small_dist=distance
-    
-print(total_distance)
+    total_distance1=total_distance1+distance[0]
+    total_distance2=total_distance2+distance[1]
+ 
+print(total_distance1)
+print(total_distance2)
 print("--- %s ms ---" % ((time.time_ns() - start_time)/1000000))
