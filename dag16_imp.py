af644f28cc6967b69148a8f1f709871794e00e99
PaulRoland <prvanderplas@gmail.com>
2023-12-16 12:51:53 +0100

    Add files via upload

--- dag16_imp.py
+++ dag16_imp.py
@@ -24,7 +24,7 @@ def laser_move(loc,direction):
         #Check bounds
         if y<0 or x<0 or y>=len(surf) or x>=len(surf[0]):
             no_split=False
-            return local_energy
+            return local_energy,(SIZE+1,SIZE+1)
         
         #energize current location
         local_energy[y][x]=1
@@ -40,16 +40,12 @@ def laser_move(loc,direction):
         if surf[y][x]==3:
             if new_direction[1] !=0: #We komen haaks op de splitter binnen
                 #print("Splitter found",y,x, (y,x) in splitter_seen)
-                loaded_energy = splitter_energy_field[(y,x)]
-                local_energy=local_energy + loaded_energy
-                return local_energy
+                return local_energy,(y,x)
                   
         if surf[y][x]==4:
             if new_direction[0] !=0: #We komen haaks op de splitter binnen
                 #print("Splitter found",y,x, (y,x) in splitter_seen)
-                loaded_energy = splitter_energy_field[(y,x)]
-                local_energy=local_energy + loaded_energy
-                return local_energy
+                return local_energy,(y,x)
                     
     return local_energy
 
@@ -156,6 +152,7 @@ def add_splitter(new_splitter,depth):
     return
 
 
+## Open de file en laad in een matrix. Houd alle splitters bij in een lijst
 splitter_list=list()
 f = open("input_dag16.txt", "r")
 SIZE=110
@@ -176,6 +173,9 @@ for i,line in enumerate(f):
             splitter_list.append([i,j,0])
 f.close()
 
+
+#Bepaal van elke splitter welke vakken hij energized en bij welke andere splitters hij uitkomt
+#dictionary om later op locatie te kunnen zoeken
 splitter_details = dict()
 for splitter in splitter_list:
     if splitter[2]==0:
@@ -185,10 +185,41 @@ for splitter in splitter_list:
         (local_energy,splitters_seen) = splitter_energy([splitter[0],splitter[1]],[1,0])
         splitter_details.update({(splitter[0],splitter[1]):[local_energy,splitters_seen]})
 
-  
-#Branch out all the splitter information
+
+########  
+#Bepaal voor elk startpunt de energized vakjes tot de eerste splitter en de locatie van de eerste splitter
+#start_energy, first_splitter
+starts=list()
+start_splitters=list()
+
+for i in range(len(surf)):
+    energy_field,start_splitter = laser_move([i,-1],[0,1]) #vanaf linkerkant
+    starts.append([energy_field,start_splitter])
+    start_splitters.append(start_splitter)
+    
+    energy_field,start_splitter =  laser_move([i,SIZE],[0,-1])
+    starts.append([energy_field,start_splitter])
+    start_splitters.append(start_splitter)
+                      
+    energy_field,start_splitter =  laser_move([-1,i],[1,0])
+    starts.append([energy_field,start_splitter])
+    start_splitters.append(start_splitter)
+                      
+    energy_field,start_splitter =  laser_move([SIZE,i],[-1,0])
+    starts.append([energy_field,start_splitter])
+    start_splitters.append(start_splitter)
+
+#Maak een lijst van unieke splitters die we het eerst tegenkomen, alleen bji die
+#Is het zinvol om de hele vertakking te bekijken
+unique_start_splitters=sorted(set(start_splitters))
+
+
+
+#Bepaal de energized vakjes als resultaat van starten bij die splitters
 splitter_energy_field = dict()
-for splitter in splitter_list:
+splitter_energy_field.update({(SIZE+1,SIZE+1):np.zeros((SIZE,SIZE))}) #Deze edge case even wegwerken
+for splitter in unique_start_splitters[:-1]:
+    
     splitter_seen=[(splitter[0],splitter[1])]
     #Kijk of de splitter uitkomt bij een splitter
     for new_splitter in splitter_details[(splitter[0],splitter[1])][1]: #details 1 heeft de nieuwe splitter
@@ -196,25 +227,22 @@ for splitter in splitter_list:
         add_splitter(new_splitter,0)
     
     
-    #print(splitter_seen)
-    loc_energy =np.zeros((SIZE,SIZE))
+    #We hebben nu een lijst van splitters die te bereiken zijn met deze splitter, zoek de totale energie
+    loc_energy = np.zeros((SIZE,SIZE))
     for splitter_key in splitter_seen:
         loc_energy = loc_energy + splitter_details[splitter_key][0]
-    
+
     splitter_energy_field.update({(splitter[0],splitter[1]):loc_energy})
     
     
 energ_list=list()
+start_list=list()
 
-for i in range(len(surf)):
-    energy_field = laser_move([i,-1],[0,1]) #vanaf linkerkant
-    energ_list.append(int(np.sum(energy_field>0)))
-    energy_field = laser_move([i,SIZE],[0,-1]) #vanaf linkerkant
-    energ_list.append(int(np.sum(energy_field>0)))
-    energy_field = laser_move([-1,i],[1,0]) #vanaf linkerkant
-    energ_list.append(int(np.sum(energy_field>0)))
-    energy_field = laser_move([SIZE,i],[-1,0]) #vanaf linkerkant
-    energ_list.append(int(np.sum(energy_field>0)))
+#Nog een keer door alle opties om de totale energie te bepalen:
+for value in starts:
+    total_energy = value[0] + splitter_energy_field[value[1]]
+    energ_list.append(np.sum(total_energy>0))
+
   
 print("Part 1", energ_list[0])
 print("Part 2", max(energ_list))
