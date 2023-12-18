f905448656ea06076b621111171521ad3981d8ec
PaulRoland <prvanderplas@gmail.com>
2023-12-18 18:18:23 +0100

    Add files via upload

--- dag18.py
+++ dag18.py
@@ -17,10 +17,15 @@ def poly_area(vertices): #Trapezoid formula
     
     for i,vertex in enumerate(vertices[:-1]):
         area += (vertex[1]+vertices[i+1][1])*(vertex[0]-vertices[i+1][0])
-    return abs(0.5*area)
+        
+    return abs(0.5*area) #linksdraaiend en rechtsdraaiend schelen een factor -1
 
 def vertices(instr):
     # key UL, UR etc   [offset_x, offset_y]
+    # Midden van de vorige beweging zit op loc_x,loc_y afhankelijk van de bocht
+    # Die wordt gemaakt in de stap daarna wordt bepaald hoe de buitencontour loopt
+    # Dit zijn de offsets voor de contour aan de linkerhand, de rechterhand is -offset.
+    # Beide bijhouden omdat je niet weet of je probleem links of rechtshandig is
     offsets={'01':[ 0.5, 0.5],
              '03':[-0.5, 0.5],
              '10':[ 0.5, 0.5],
@@ -29,22 +34,28 @@ def vertices(instr):
              '23':[-0.5,-0.5],
              '30':[-0.5, 0.5],
              '32':[-0.5,-0.5]}
+    
     #directions key: xy
     directions={'0':[1,0],'1':[0,-1],'2':[-1,0],'3':[0,1]}
                              
-    #Add one to the list to fix the beginning         
-    instr.append(instr[0])
+
     
     #Left handed and right handed list
     vert_L = list()
     vert_R = list() 
     
+    #Bereken de locatie van na de eerste stap en bepaal de previous direction
     prev_direction=instr[0][0]
     loc_x=instr[0][1]*directions[instr[0][0]][0] #length * direction
     loc_y=instr[0][1]*directions[instr[0][0]][1]
     
+    #Add one to the list to fix for the beginning         
+    instr.append(instr[0])
+    
+    #Ga nu alle stappen af, skip de eerste, die zit nu aan het einde
     for [direction,length] in instr[1:]:
         offset_key=prev_direction+direction
+        
         vert_L.append([loc_x+offsets[offset_key][0],loc_y+offsets[offset_key][1]])
         vert_R.append([loc_x-offsets[offset_key][0],loc_y-offsets[offset_key][1]])
         prev_direction=direction
@@ -54,7 +65,7 @@ def vertices(instr):
     return vert_L,vert_R
 
 
-directions={'U':'3','D':'1','L':'2','R':'0'}
+directions_trans={'U':'3','D':'1','L':'2','R':'0'}
 f = open("input_dag18.txt", "r")
 
 instr_p1=list()
@@ -63,7 +74,7 @@ instr_p2=list()
 for i,line in enumerate(f):
     line=line.replace('(','').replace(')','').replace('=','').replace('\n','')
     [direction,length,color] =line.split(' ')
-    instr_p1.append([directions[direction],int(length)])
+    instr_p1.append([directions_trans[direction],int(length)])
     instr_p2.append([color[-1],int(color[1:-1],16)])
 f.close()
 
